7a911e34128993cf047b0d4c41c34f24c221cab7
Mrinal561 <mrinal@mobotics.in>
2024-10-07 15:25:36 +0530

    update

--- src/views/IHRC/components/GlobalSettings/VersionHistory/components/VersionHistoryTable.tsx
+++ src/views/IHRC/components/GlobalSettings/VersionHistory/components/VersionHistoryTable.tsx
@@ -20,22 +20,22 @@ const initialData: VersionHistoryDataRow[] = [
     },
     {
         versionNumber: '1.1',
-        updatedDate: '2022-01-01',
+        updatedDate: '2022-02-10',
         changeLog: 'Initial release.',
     },
     {
         versionNumber: '1.2',
-        updatedDate: '2022-01-01',
+        updatedDate: '2022-03-15',
         changeLog: 'Initial release.',
     },
     {
         versionNumber: '1.3',
-        updatedDate: '2022-01-01',
+        updatedDate: '2022-04-22',
         changeLog: 'Initial release.',
     },
     {
         versionNumber: '1.4',
-        updatedDate: '2022-01-01',
+        updatedDate: '2022-05-18',
         changeLog: 'Initial release.',
     },
   
