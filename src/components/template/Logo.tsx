ebc6c686bf55c74356ae782f140a14862c87adb3
Mrinal561 <mrinal@mobotics.in>
2025-04-12 23:39:55 +0530

    fixed logo path

--- src/components/template/Logo.tsx
+++ src/components/template/Logo.tsx
@@ -32,7 +32,7 @@ const Logo = (props: LogoProps) => {
         >
             <img
                 className={imgClass}
-                src={`${LOGO_SRC_PATH}logo-${mode}-${type}.png`}
+                src={`${LOGO_SRC_PATH}.png`}
                 alt={`${APP_NAME} logo`}
             />
         </div>
